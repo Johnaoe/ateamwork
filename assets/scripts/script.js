c9095ebe424eea0aff00a0d7650c0c84c10c7eab
Johnaoe <88572943+Johnaoe@users.noreply.github.com>
2022-12-07 19:50:49 +0200

    script-part

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,4 +10,31 @@ buttons.forEach((button) => {
         infoscreen.classList.toggle("show-content")
     })
 });
+// Side menu element toggle bellow -->
+const btn = document.querySelector('#btn');
+const info = document.querySelector('.side_menu__container')
 
+btn.addEventListener("click", (ea) => {
+    info.classList.toggle('show-element');
+});
+
+const close_btn = document.querySelector('#close_btn');
+const info2 = document.querySelector('.side_menu__container')
+
+close_btn.addEventListener("click", (ea) => {
+    info2.classList.toggle('show-element');
+});
+// Background element toggle bellow -->
+const bg_btn = document.querySelector('#btn');
+const bg_info = document.querySelector('.bg_side_menu__container')
+
+bg_btn.addEventListener("click", (ea) => {
+    bg_info.classList.toggle('view-element');
+});
+
+const close_bg_btn = document.querySelector('#close_btn');
+const close_bg_info = document.querySelector('.bg_side_menu__container')
+
+close_bg_btn.addEventListener("click", (ea) => {
+    close_bg_info.classList.toggle('view-element');
+});
